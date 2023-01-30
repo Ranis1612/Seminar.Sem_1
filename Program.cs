88b7988f913666d7f8035870c0d9b6829bc51620
Ranis1612 <Ranis1612@mail.ru>
2023-01-30 21:07:12 +0500

    Added file gitignore

---
+++ Program.cs
@@ -0,0 +1,7 @@
+﻿// Напишите программу, которая на вход принимает число и выдает его квадрат (число умноженное на само себя).
+//Например: 4 -> 16; -3 -> 9; -7 -> 49
+Console.Clear();
+Console.Write("Введите число:  ");
+int number = int.Parse(Console.ReadLine());
+int quatro = number * number;
+Console.WriteLine($"Квадрат числа{number} равен {quatro}");
